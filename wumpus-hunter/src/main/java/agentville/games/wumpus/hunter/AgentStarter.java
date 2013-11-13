65bc3f2d18c661898d8fbe0c11f30900f6cd08a8
Marco <marco.steffens@gmail.com>
2013-11-13 16:41:33 +0100

    init

---
+++ wumpus-hunter/src/main/java/agentville/games/wumpus/hunter/AgentStarter.java
@@ -0,0 +1,56 @@
+package agentville.games.wumpus.hunter;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+
+public class AgentStarter {
+
+	public static void main(String[] args){
+	
+		String host;
+		int port;
+		String platform = null;		//default name
+		boolean main = true;
+	
+		host = "localhost";
+		port = -1;			//default-port 1099
+	
+		Runtime runtime = Runtime.instance();
+	
+		Profile profile = null;
+		AgentContainer container = null;
+	
+		profile = new ProfileImpl(host, port, platform, main);
+	
+		//Container erzeugen
+		container = runtime.createMainContainer(profile);
+
+//		Agent rmagent = new jade.tools.rma.rma();
+//		// Remote Monitoring Agenten erzeugen
+//		try {
+//			AgentController rma = container.acceptNewAgent(
+//	                	"RMA",
+//	                    rmagent
+//	                    );
+//			rma.start();
+//		} catch(StaleProxyException e) {
+//			throw new RuntimeException(e);
+//		}
+	
+
+		// Agenten erzeugen und startet - oder aussteigen.
+		try {
+			AgentController agent = container.createNewAgent(
+	                	"MyAgent",
+	                    MyAgent.class.getName(),
+	                    args);
+			agent.start();
+	    } catch(StaleProxyException e) {
+	        throw new RuntimeException(e);
+	    }
+	}
+}
---
+++ wumpus-hunter/src/main/java/agentville/games/wumpus/hunter/MyAgent.java
@@ -0,0 +1,26 @@
+package agentville.games.wumpus.hunter;
+
+import jade.core.Agent;
+import jade.util.Logger;
+
+public class MyAgent extends Agent {
+	
+//	Logger logger = jade.util.Logger.getMyLogger(this.getClass().getName());	
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void setup() {
+		
+		try {
+		    Thread.sleep(10000);
+		} catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
+		
+//		if (logger.isLoggable(jade.util.Logger.WARNING))
+//			logger.log(jade.util.Logger.WARNING, "greetings from setup()");		
+	    	
+        System.out.println("Hello Eclipse! This is "
+                + getLocalName());
+    }
+}
